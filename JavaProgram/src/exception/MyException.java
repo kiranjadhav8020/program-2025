b130b08e42a72643efddd0924e3dfb6869df569f
Kiran Jadhav <kiranjadhav8020@gmail.com>
2025-06-24 12:46:47 +0530

    project added

---
+++ JavaProgram/src/exception/MyException.java
@@ -0,0 +1,12 @@
+package exception;
+
+public class MyException {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("My Exception");
+
+	}
+
+}
---
+++ JavaProgram/src/opps/MyOppsProgram.java
@@ -0,0 +1,12 @@
+package opps;
+
+public class MyOppsProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("My opps Program");
+
+	}
+
+}
