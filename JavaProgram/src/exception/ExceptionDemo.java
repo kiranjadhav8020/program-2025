af3eea05a06338eede076dac7cb4963719ddb1be
Anita <anitanikam808080@gmail.com>
2025-06-26 13:23:43 +0530

    Exception Program

---
+++ JavaProgram/src/exception/ExceptionDemo.java
@@ -0,0 +1,21 @@
+public class ExceptionDemo{
+   
+   public static void main(String [] args){
+      
+	  int a=10;
+      int b=0;
+	  
+	  try{
+	  
+	   int result=a/b;
+	   System.out.println("Result:"+result);
+	  }
+	  catch(ArithmeticException e){
+	  
+	    System.out.println("Error:can not divided by zero");
+	    
+	  }
+      
+   }
+  
+}
