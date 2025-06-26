d8711addaa491ba4a73bf20e286bfe76375c2a3f
Kiran Jadhav <kiranjadhav8020@gmail.com>
2025-06-26 14:32:21 +0530

    change code in DefaultException class

--- JavaProgram/src/exception/DefaultException.java
+++ JavaProgram/src/exception/DefaultException.java
@@ -1,14 +1,41 @@
   public class DefaultException{
   
     public static void main (String [] args){
-	
-	   int a=10;
-	   int b=0;
-	   
-	   int result=a/b;
-	   
-	   System.out.println(result);
+		
+		DefaultException d1=new DefaultException();
+		
+		d1.test1(0);
+		
+	  try {
+		  
+            d1.test2(16); 
+			
+        } catch (ArithmeticException e) {
+			
+            System.out.println("Exception caught: " + e.getMessage());
+        }
 	
 	}
+	
+	public void test1(int a){
+		
+			try{
+				
+				int result=a/2;
+				
+			}catch(ArithmeticException e){
+				
+				System.out.println("Exception accured due to divided by 0 ");
+				
+			}
+		
+	}
+	
+	 // This method declares it might throw an exception
+    public static void test2(int a) throws ArithmeticException {
+        
+			int result = a/2;
+			
+    }
   
   }
