6dd6dca99fdd96dfa1f8582b45bb610b5ff37c99
Anita <anitanikam808080@gmail.com>
2025-06-26 13:37:35 +0530

    Default exception

---
+++ JavaProgram/src/exception/DefaultException.java
@@ -0,0 +1,14 @@
+  public class DefaultException{
+  
+    public static void main (String [] args){
+	
+	   int a=10;
+	   int b=0;
+	   
+	   int result=a/b;
+	   
+	   System.out.println(result);
+	
+	}
+  
+  }
